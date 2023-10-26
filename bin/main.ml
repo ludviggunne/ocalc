fe44cb9dfe91e288baa7b1ccf3ea9b821123d3a0
Ludvig Gunne Lindström <ludvig.gunne.lindstrom@proton.me>
2023-10-26 19:58:30 +0200

    implement cli

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,13 @@
 
-let lexer = Lexer.init "10 * - 2"
-let _, ast = Parser.parse lexer
-let () = Printf.printf "%d\n" (Ast.eval ast)
+let rec loop () =
+  let _ = Printf.printf "> " in
+  let line = read_line () in
+  match line with
+  | "" -> ()
+  | line ->
+    let lexer = Lexer.init line in
+    let _, ast = Parser.parse lexer in
+    let _ = Printf.printf "= %d\n" (Ast.eval ast) in
+    loop ()
+
+let _ = loop ()
--- bin/parser.ml
+++ bin/parser.ml
@@ -1,51 +1,52 @@
 
 open Token
+open Lexer
 open Ast
 
 let rec parse lexer = sum lexer
 
 and sum lexer =
   let rec take lexer left =
-    match Lexer.next lexer with
+    match next lexer with
     | _, None -> lexer, left
-    | lexer, Some tok ->
+    | next_lexer, Some tok ->
       match tok with
       | Plus | Minus ->
-        let lexer, right = product lexer in
-        take lexer (Binary (left, tok, right))
+        let next_lexer, right = product next_lexer in
+        take next_lexer (Binary (left, tok, right))
       | _ -> lexer, left
   in let lexer, left = product lexer
   in take lexer left
 
 and product lexer =
   let rec take lexer left =
-    match Lexer.next lexer with
+    match next lexer with
     | _, None -> lexer, left
-    | lexer, Some tok ->
+    | next_lexer, Some tok ->
       match tok with
       | Asterix | Slash ->
-        let lexer, right = factor lexer in
-        take lexer (Binary (left, tok, right))
+        let next_lexer, right = factor next_lexer in
+        take next_lexer (Binary (left, tok, right))
       | _ -> lexer, left
   in let lexer, left = factor lexer
   in take lexer left
 
 and factor lexer =
-  match Lexer.next lexer with
+  match next lexer with
   | _, None -> failwith "Empty expression"
-  | lexer, Some tok ->
+  | next_lexer, Some tok ->
     match tok with
-    | Num x -> lexer, Atomic x
+    | Num x -> next_lexer, Atomic x
     | Minus ->
-      let lexer, operand = factor lexer in
-      lexer, Neg operand
+      let next_lexer, operand = factor next_lexer in
+      next_lexer, Neg operand
     | LeftParenthesis ->
-      (let lexer, expr = sum lexer in
-      match Lexer.next lexer with
+      (let next_lexer, expr = sum next_lexer in
+      match next next_lexer with
       | _, None -> failwith "Missing closing parenthesis"
-      | lexer, Some tok ->
+      | next_lexer, Some tok ->
         (match tok with
-        | RightParenthesis -> lexer, expr
+        | RightParenthesis -> next_lexer, expr
         | _ -> failwith "Missing closing parenthesis"))
     | _ -> failwith "Unexpected token"
 ;;
